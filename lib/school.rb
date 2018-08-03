305037c10e1ec2b13ac1028c28fb6ba1b7107237
Chris Franklin <cf73021@gmail.com>
2018-08-03 15:35:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,10 @@ class School
   end
 
   def add_student(name, grade)
-    ROSTER[grade] ||= [] 
+    ROSTER[grade] ||= []
     ROSTER[grade] << name
   end
+
+  def grade(grade)
+    roster[grade
 end
