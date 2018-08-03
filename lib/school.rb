7d745200b548bd3f7370efa13e82ac525623764c
Chris Franklin <cf73021@gmail.com>
2018-08-03 15:27:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  ROSTER = {}
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    ROSTER
+  end
+
+  def add_student(name, grade)
+    ROSTER[grade] = [] if ROSTER[grade] == nil
+    ROSTER[grade] << name
+  end
+end
